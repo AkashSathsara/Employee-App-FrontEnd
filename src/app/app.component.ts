6a8f5625a5a6c477d2be2932fec85c18d5abad3a
AkashSathsara <akashsathsara10@gmail.com>
2025-02-02 14:13:50 +0000

    add-employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ManageEmployeeComponent } from './pages/manage-employee/manage-employee.component';
+import { AddEmployeeComponent } from './pages/add-employee/add-employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ManageEmployeeComponent,AddEmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
