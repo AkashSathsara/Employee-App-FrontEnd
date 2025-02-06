0173ca107cd33101ea6223f0e7d93adb9c8daac3
AkashSathsara <akashsathsara10@gmail.com>
2025-02-06 08:13:24 +0000

    view-employee page

--- src/app/pages/manage-employee/manage-employee.component.ts
+++ src/app/pages/manage-employee/manage-employee.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-manage-employee',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HttpClientModule,CommonModule],
   templateUrl: './manage-employee.component.html',
   styleUrl: './manage-employee.component.css'
 })
 export class ManageEmployeeComponent {
 
+ public employeeObj ={
+  firstName :"",
+  lastName:"",
+  email:"",
+  departmentId:"",
+  roleId:""
+ }
+
+ constructor(private http:HttpClient){}
+
+addEmployee(){
+  this.http.post("http://localHost:8080/emp-controller/add-employee",this.employeeObj).subscribe(
+    (data) =>{
+      Swal.fire({
+        title: "Employee Add!",
+        text: "You clicked the button!",
+        icon: "success"
+      });
+    }
+  )
+
+}
 }
